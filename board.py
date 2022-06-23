77b3cbaca535857d465359d1a718e0b0ee922f9e
Benjamin Kleyner <benkleyner@gmail.com>
2022-06-23 14:43:41 -0400

    Board printing funcitonality added

---
+++ board.py
@@ -0,0 +1,12 @@
+#Tic-Tac_Toe
+
+def printBoard(s):
+    print("Printing the board...")
+    for i in s:
+        print(i)
+
+board = [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-']]
+
+printBoard(board)
